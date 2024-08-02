265c06088326e6ff781dd3f3da517afb7aab1c10
Danniel Navas <dannielnavas@gmail.com>
2024-08-02 07:18:58 -0500

    ✨ chore: Update npm dependencies and server configuration

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world! 🌍");
+import express, { Express, Request, Response } from "express";
+import { config } from "./config";
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("<h1>Hola mundo con algo</h1>");
+});
+
+app.listen(config.port, () => {
+  console.log(`Server is running on http://localhost:${config.port}`);
+});
