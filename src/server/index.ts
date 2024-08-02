e4abd8d71e699848e500e667be410a4998a6fb11
Danniel Navas <dannielnavas@gmail.com>
2024-08-02 07:26:21 -0500

    ✨ feat: Add template rendering for server response

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import { config } from "./config";
+import { template } from "./render/template";
 
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("<h1>Hola mundo con algo</h1>");
+  res.send(template(`<h1>Hello, World! ${req.url}</h1>`));
 });
 
 app.listen(config.port, () => {
